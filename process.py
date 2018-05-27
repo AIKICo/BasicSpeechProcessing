6f5f14c47d4dc818a010ec7ad0988c7778ab76c5
MohMehrnia <qermezkon@gmail.com>
2018-05-27 12:10:00 +0430

    plot wave file

--- process.py
+++ process.py
@@ -1,4 +1,25 @@
+import os
+import sys
 import numpy
-import scipy.io.wavfile
+import scipy.io.wavfile as wavfile
+import matplotlib.pyplot as plt
 from scipy.fftpack import dct
 
+
+def readWave(inputFilename):    
+    sample_rate, signal = wavfile.read(inputFilename)
+    signal = signal[0:int(3.5 * sample_rate)] # 3.5 Search From Start
+    return sample_rate, signal
+
+def plotWave(inputSignal, inputSample_Rate,inputfilename):
+    x_value = numpy.arange(0, len(inputSignal), 1) / float(inputSample_Rate)    
+    plt.plot(x_value, inputSignal)    
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.title(inputfilename)
+    plt.show()
+
+if __name__ == "__main__":
+    _filename = sys.argv[1]
+    _sample_rate, _signal = readWave(_filename)
+    plotWave(_signal, _sample_rate, _filename)

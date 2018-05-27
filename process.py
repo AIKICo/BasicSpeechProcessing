f89f961f21c0566a2dd1b9515c36918e2b460308
MohMehrnia <qermezkon@gmail.com>
2018-05-27 13:54:13 +0430

    Pre Emphasis Filter

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import os
-import sys
-import numpy
+import sys 
+import numpy as np
 import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
 from scipy.fftpack import dct
@@ -8,15 +8,20 @@ from scipy.fftpack import dct
 
 def readWave(inputFilename):    
     sample_rate, signal = wavfile.read(inputFilename)
-    signal = signal[0:int(3.5 * sample_rate)] # 3.5 Search From Start
+    signal = signal[0:int(3.5 * sample_rate)] # 3.5 (s) From Start
     return sample_rate, signal
 
+def preEmphasis(inputSignal ,inputAlpha):
+    signal = np.append(signal[0], signal[1:] - inputAlpha * signal[:-1]) # y(y) = x(t) - Apha*X(t-1)
+
 def plotWave(inputSignal, inputSample_Rate,inputfilename):
-    x_value = numpy.arange(0, len(inputSignal), 1) / float(inputSample_Rate)    
+    x_value = np.arange(0, len(inputSignal), 1) / float(inputSample_Rate)    
     plt.plot(x_value, inputSignal)    
     plt.ylabel("Amplitude")
     plt.xlabel("Time (s)")
     plt.title(inputfilename)
+    plt.box("off")
+    plt.grid(True)
     plt.show()
 
 if __name__ == "__main__":

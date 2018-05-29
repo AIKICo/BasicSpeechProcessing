138e386d9deb039baf7c4b227e93ab8b64876498
Mohammad Mehrnia <qermezkon@gmail.com>
2018-05-29 13:15:16 +0430

    Final Speech Basic Process

--- process.py
+++ process.py
@@ -51,6 +51,36 @@ def convert_frames_fft(frames_, NFTT_):
 def frame_power_spectrum(mag_frames_, NFTT_):
     return ((1.0 / NFTT_) * (mag_frames_ ** 2))
 
+def fb(pow_spectrum_,sample_rate_, NFFT_, nfilt_):
+    low_freq_mel = 0
+    high_freq_mel = (2595 * np.log10(1 + (sample_rate_ / 2) / 700))  # Convert Hz to Mel
+    mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt_ + 2)  # Equally spaced in Mel scale
+    hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+    _bin = np.floor((NFFT_ + 1) * hz_points / sample_rate_)
+
+    fbank = np.zeros((nfilt_, int(np.floor(NFFT_ / 2 + 1))))
+    for m in range(1, nfilt_ + 1):
+        f_m_minus = int(_bin[m - 1])   # left
+        f_m = int(_bin[m])             # centersample_rate_
+        f_m_plus = int(_bin[m + 1])    # right
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - _bin[m - 1]) / (_bin[m] - _bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (_bin[m + 1] - k) / (_bin[m + 1] - _bin[m])
+    filter_banks = np.dot(pow_spectrum_, fbank.T)
+    filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)  # Numerical Stability
+    filter_banks = 20 * np.log10(filter_banks)  # dB
+    return filter_banks
+
+def mfcc(filter_bank_, num_ceps_, cep_lifter_):
+    mfccframes = dct(filter_bank_, type=2, axis=1, norm='ortho')[:, 1 : (num_ceps_ + 1)]
+    (nframes, ncoeff) = mfccframes.shape
+    n = np.arange(ncoeff)
+    lift = 1 + (cep_lifter_ / 2) * np.sin(np.pi * n / cep_lifter_)
+    mfccframes *= lift 
+    return mfccframes
+
 if __name__ == "__main__":
     _filename = sys.argv[1]
     _sample_rate, _signal = readWave(_filename)
@@ -61,3 +91,6 @@ if __name__ == "__main__":
 
     _mag_frames, _NFTT = convert_frames_fft(_frames, 512)
     _pow_spectrum = frame_power_spectrum(_mag_frames, _NFTT)
+    _filter_banks = fb(_pow_spectrum, _sample_rate, _NFTT, 40)
+    _mfcc = mfcc(_filter_banks, 12, 40)    
+    print(_mfcc[0])
